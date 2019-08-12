9d96a9b84b8fcd679fffc37679d629dd43fdf44d
Abel Ponce <vnponce8@gmail.com>
2019-08-12 18:39:53 -0500

    working with tailwindcss and mix

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {}
+  },
+    variants: {
+        backgroundColor: ['responsive', 'odd', 'even'],
+        order: ['responsive', 'hover', 'focus', 'odd', 'even'],
+    },
+  plugins: []
+}
