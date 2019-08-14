401fc01fd5dd67b031c04a71c31242045b45340b
Abel Ponce <vnponce8@gmail.com>
2019-08-14 15:35:35 -0500

    wip product portrait table

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Product page')
 
 @section('content')
+    <div class="flex flex-col mt-12 sm:mt-16 sm:flex-row">
+        <!-- Back button -->
+        <svg class="w-8 h-8 mb-4 ml-4 text-lg cursor-pointer sm:w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+        <!-- Product -->
         <div class="flex flex-col pb-16">
             <!-- Image -->
             <div class="bg-gray-100 sm:w-2/3 sm:m-auto">
@@ -49,4 +53,5 @@
                 </div>
             </div>
         </div>
+    </div>
 @stop
