687ec7a0dd44dacf3adef5130393712adb38d5c2
Abel Ponce <vnponce8@gmail.com>
2019-10-15 21:11:01 -0500

    adjustin colors

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -23,7 +23,7 @@
                 <div class="mb-6">
                     <div class="flex">
                         <div class="flex items-center flex-1 inline-block">
-                            <svg class="cursor-pointer -ml-5 mr-2 stroke-current fill-current  text-regularText w-3"  viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <svg class="cursor-pointer -ml-5 mr-2 stroke-current fill-current  text-gray-500 w-3"  viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                 <polygon points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644"></polygon>
                             </svg>
                             <span class="ml-0 text-lg">Producto</span>
@@ -53,12 +53,12 @@
                 <!-- 'Proceder' Next step -->
                 <div class="flex items-center justify-center">
                     <input id="checkout-agree" class="mr-2" type="checkbox">
-                    <label for="checkout-agree" class="text-gray-500 text-base italic">
+                    <label for="checkout-agree" class="text-center text-gray-500 text-base italic">
                         ¿El contenido de tu compra es el correcto?
                     </label>
                 </div>
                 <!-- Paypal button or levantar pedido si es usuario -->
-                <button class="flex cursor-pointer justify-center font-bold py-2 px-4 rounded w-1/2 m-auto mt-4 block bg-transparent border border-brand-orange text-brand-orange text-bold hover:bg-brand-orange hover:text-white hover:shadow hover:text-white">
+                <button class="flex cursor-pointer justify-center font-bold py-2 px-4 rounded w-full md:w-1/2 m-auto mt-4 block bg-transparent border border-brand-orange text-brand-orange text-bold hover:bg-brand-orange hover:text-white hover:shadow hover:text-white">
                     <span>Paypal</span>
                 </button>
             </div>
