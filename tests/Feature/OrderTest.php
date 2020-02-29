f65b0e161fec7e672d8022a953bc863e2ba1e100
Abel Ponce <vnponce8@gmail.com>
2020-02-29 11:36:53 -0600

    first test pass

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Features;
 
+use App\Category;
 use App\Order;
 use App\Product;
 use App\Store;
@@ -9,13 +10,14 @@ use App\User;
 use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Support\Facades\Mail;
+use Jenssegers\Date\Date;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class OrderTest extends TestCase
 {
-    use RefreshDatabase;
+     use RefreshDatabase;
 
     /** @test */
     function it_send_email_to_user_with_data()
@@ -48,6 +50,21 @@ class OrderTest extends TestCase
     /** @test */
     function it_show_orders()
     {
+        Cart::destroy();
+        $category = factory(Category::class)->create([
+            'name' => 'Pan de Dulce',
+        ]);
+        $product = factory(Product::class)->create([
+            "name" => "Hojaldra de jamón y queso",
+            "description" => "1 pieza",
+            "ingredients" => "jamón",
+            "image" => "public/products/plZXSjboICXf9sUaR888t4kK8ln7dWwZmYOXr9N8.jpeg",
+            "price" => 70000,
+            "available" => 1,
+            "available_time" => null,
+            "favorite" => 1,
+            "category_id" => $category->id,
+        ]);
         $user = User::create([
             'name' => 'Super Sayayin Dios',
             'email' => 'god@panaderialaespecial.mx',
@@ -67,28 +84,86 @@ class OrderTest extends TestCase
             "monday_saturday" =>"Lunes a sábado",
             "sunday" =>"Cierra",
         ]);
+//        $date = Carbon::now();
+        $date = new Date();  // Para tener la fecha en español
         $order = Order::create([
             'store_id' => $store->id,
-            'date' => Carbon::now(),
+            'date' => $date,
             'hour' => '11:00',
             'name' => 'Nombre',
             'lastname' => 'Apellido',
             'phone' => '1222312',
             'email' => 'email@test.com',
             'employeeName' => 'nombreEmpleado',
-            'total' => '200',
+            'total' => 2000,
             // @todo: hacer status
             'status' => 'created',
         ]);
 
+        $order->products()->attach($product->id, [
+            'price' => $product->price * 100,
+            'quantity' => 2,
+            'comment' => $product->options->comment ?? '',
+        ]);
+
         $response = $this->get('/admin')->assertSee('Nombre');
         $this->assertDatabaseHas('orders', $order->toArray());
-        dd($response->getOriginalContent()->getData());
-        dd($response->getOriginalContent()->getData()['page']['props']);
+        // dd($response->getOriginalContent()->getData());
+        // dd($response->getOriginalContent()->getData()['page']['props']);
         $this->get('/admin')
             ->assertStatus(200)
-            ->assertPropValue('filters.trashed', 'with')
-            ->assertPropCount('contacts.data', 5);
+            ->assertPropCount('orders', 1)
+            ->assertPropValue('orders', function($orders) use($date){
+                // dd($order[0]);
+                $order = Order::first();
+                $products = $order->products->map(function($product){
+                    return [
+                        'name' => $product->name,
+                        'qty' => $product->pivot->quantity,
+                        'price' => $product->pivot->price / 100,
+                        'options' => [
+                            'comment' => $product->pivot->comment,
+                        ],
+                    ];
+                });
+//                dd($products->toArray());
+//                dd($order->products);
+                $this->assertEquals([
+                    'id' => $order->id,
+                    'uuid' => $order->uuid,
+                    'date' =>  [
+                        'formatted' => $date->format('D d M, H:ia'),
+                        // This is a hack, because this date in a way is returning correct time zone but idk why is transformed in other timezone.
+                        // This must be after formatted because it change timezone and it chang values.
+                        'original' => $date->timezone('UTC')->format('Y-m-d\TH:i:s.\0\0\0\0\0\0\Z'),
+                        'forToday' => $date->isToday(),
+                        'forTomorrow' => $date->isTomorrow(),
+                    ],
+                    'customer' => [
+                        'name' => 'Nombre Apellido',
+                        'email' => 'email@test.com',
+                        'phone' => '1222312',
+                    ],
+                    'store' => [
+                        'friendlyAddress' => "Bernal Díaz del Castillo entre Isabel La Católica y Martí, Fracc. Reforma",
+                        'name' => 'Bernal',
+                        'isMatrix' => false,
+                    ],
+                    'products' => $products->toArray(),
+                    'status' => [
+                        'original' => $order->status,
+                        'step' => $order->statusStep,
+                    ],
+                    'payed' => [
+                        'original' => false,
+                        'label' => 'Pendiente',
+                    ],
+                    'employee' => [
+                        'name' => 'nombreEmpleado',
+                    ],
+                    'total' => toFormat($order->total),
+                ], $orders[0]);
+            });
         // dd($response);
     }
 }
