8502567bffeae82e26414a9cbd2d3e18420494eb
Abel Ponce <vnponce8@gmail.com>
2020-11-19 10:31:26 -0600

    creating product

---
+++ tests/Feature/ProductsTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+
+class ProductsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_creates_product()
+    {
+        Storage::fake('photos');
+
+        $this->withoutExceptionHandling();
+        $category = factory(\App\Category::class)->create();
+//        dd($category->toArray());
+        $user = factory(User::class)->create();
+//        dd($user->toArray())
+
+
+        $this->actingAs($user)
+            ->post('/admin/products/', [
+            "name" => "Hojaldra de jamón y queso",
+            "description" => "1 pieza",
+            "ingredients" => "jamón",
+//            "file" => "public/products/plZXSjboICXf9sUaR888t4kK8ln7dWwZmYOXr9N8.jpeg",
+            "file" => UploadedFile::fake()->image('avatar.jpg'),
+            "price" => 8,
+            "available" => 1,
+            "available_time" => null,
+            "favorite" => 1,
+            "category_id" => $category->id,
+        ]);
+
+        $this->assertDatabaseHas('products', [
+            "name" => "Hojaldra de jamón y queso",
+            "description" => "1 pieza",
+            "ingredients" => "jamón",
+//            "image" => "public/products/plZXSjboICXf9sUaR888t4kK8ln7dWwZmYOXr9N8.jpeg",
+            "price" => 800,
+            "available" => 1,
+            "available_time" => null,
+            "favorite" => 1,
+        ]);
+    }
+}
