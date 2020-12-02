a3defba44cc72dcb4bba1f204c067353f1ccf4f9
Abel Ponce <vnponce8@gmail.com>
2020-12-01 23:34:42 -0600

    refactoring test locations

---
+++ tests/Feature/Admin/SuggestionsTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Suggestion;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SuggestionsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_can_show_suggestions_list()
+    {
+//        $this->withoutExceptionHandling();
+        factory(Suggestion::class, 4)->create();
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get("/admin/suggestions");
+        $response->assertStatus(200)
+            ->assertPropCount('suggestions', 4);
+//            ->assertPropValue('suggestions', function($orders) use ($orderToDeliverTomorrow){
+//                $filteredOrder = collect($orders)->first();
+//                $this->assertEquals($orderToDeliverTomorrow->uuid, collect($filteredOrder)->get('uuid'));
+//            });
+    }
+
+    /** @test */
+    function logged_user_can_update_status_to_viewed()
+    {
+        $this->withoutExceptionHandling();
+
+        $suggestion = factory(Suggestion::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
+            'status' => 'viewed',
+        ]);
+
+        $this->assertEquals('viewed', Suggestion::first()->status);
+    }
+
+    /** @test */
+    function it_only_accept_viewed_solved_as_status_values()
+    {
+        $this->withExceptionHandling();
+
+        $suggestion = factory(Suggestion::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
+            'status' => 'viewed',
+        ]);
+        $this->assertEquals('viewed', Suggestion::first()->status);
+
+        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
+            'status' => 'solved',
+        ]);
+        $this->assertEquals('solved', Suggestion::first()->status);
+
+        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
+            'status' => 'not-allowed-status',
+        ])->assertSessionHasErrors('status');
+    }
+}
--- tests/Feature/SuggestionsTest.php
+++ tests/Feature/SuggestionsTest.php
@@ -13,22 +13,6 @@ class SuggestionsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function it_can_show_suggestions_list()
-    {
-//        $this->withoutExceptionHandling();
-        factory(Suggestion::class, 4)->create();
-        $user = factory(User::class)->create();
-
-        $response = $this->actingAs($user)->get("/admin/suggestions");
-        $response->assertStatus(200)
-            ->assertPropCount('suggestions', 4);
-//            ->assertPropValue('suggestions', function($orders) use ($orderToDeliverTomorrow){
-//                $filteredOrder = collect($orders)->first();
-//                $this->assertEquals($orderToDeliverTomorrow->uuid, collect($filteredOrder)->get('uuid'));
-//            });
-    }
-
-    /** @test */
     function it_can_store_suggestions()
     {
         $this->withoutExceptionHandling();
@@ -76,44 +60,6 @@ class SuggestionsTest extends TestCase
     {
         $this->suggestion($this->validFields(['suggestion' => '']))
             ->assertSessionHasErrors('suggestion');
-    }
-
-    /** @test */
-    function logged_user_can_update_status_to_viewed()
-    {
-        $this->withoutExceptionHandling();
-
-        $suggestion = factory(Suggestion::class)->create();
-        $user = factory(User::class)->create();
-
-        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
-            'status' => 'viewed',
-        ]);
-
-        $this->assertEquals('viewed', Suggestion::first()->status);
-    }
-
-    /** @test */
-    function it_only_accept_viewed_solved_as_status_values()
-    {
-        $this->withExceptionHandling();
-
-        $suggestion = factory(Suggestion::class)->create();
-        $user = factory(User::class)->create();
-
-        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
-            'status' => 'viewed',
-        ]);
-        $this->assertEquals('viewed', Suggestion::first()->status);
-
-        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
-            'status' => 'solved',
-        ]);
-        $this->assertEquals('solved', Suggestion::first()->status);
-
-        $this->actingAs($user)->put("/admin/suggestions/{$suggestion->id}/updateStatus", [
-            'status' => 'not-allowed-status',
-        ])->assertSessionHasErrors('status');
     }
 
     protected function suggestion($attributes = [])
